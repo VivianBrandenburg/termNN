8d59a4607939361a53ae122bd3133b6f3f7b7651
Vivian Brandenburg <code@vivian-brandenburg.de>
2021-12-21 14:47:10 +0100

    updated structure

---
+++ results/terminators/genomescan/encoded_genome/readme.md
@@ -0,0 +1,3 @@
+# where is the encoded genome?
+
+Due to its large size, the encoded genome has not been uploaded. You can create the files running genome_scan.py. 
--- scan_genome.py
+++ scan_genome.py
@@ -2,13 +2,17 @@
 
 from termNN_tools import genomescan , plots
 from termNN_tools.toolbox import check_and_make_dir
+import sys
 
+
+mode = sys.argv[1]
 
 # dirs
 DIR_genomic = 'data/terminators/ecoli_genome/'
 DIR_models = 'models/terminators/'
 DIR_res = 'results/terminators/genomescan/'
 FILE_arnold = 'data/arnold/genomescan_ecoli_processed.csv'
+DIR_PPV = DIR_res+'PPV/'
 
 # variables
 splits = 50
@@ -24,32 +28,25 @@ gff= genomescan.get_gff(DIR_genomic + 'GCF_000005845.2_ASM584v2_genomic.gff')
 for reverse, s in zip([False, True], ['fwd/', 'rev/']):
             
     # strand-specific files and dirs
-    DIR_encode =  DIR_res+'encoded/'+s
-    DIR_res_s =  DIR_res+'all_results/'+s
-    DIR_kernels = DIR_res+'kernels/'+s
+    DIR_encode =  DIR_res+'encoded_genome/'+s
+    DIR_res_s =  DIR_res+'results_single_nts/'+s
+    DIR_kernels = DIR_res+'results_kernels/'+s
     check_and_make_dir(DIR_encode, DIR_res_s, DIR_kernels)
     
 
-
-# #### the following steps are time-consuming!
-# #### un-comment them if you want to start the genome-scan from scratch 
-# #### >>> 
-
-#     # scan for terminators in genome  
-#     genomescan.encode_genome(genome, gff, splits, DIR_encode, reverse)
-#     genomescan.scan_genome(splits, ks, DIR_encode, DIR_models, DIR_res_s)
+    # scan for terminators in genome  
+    if mode == 'encode_genome': genomescan.encode_genome(genome, gff, splits, DIR_encode, reverse)
+    if mode == 'scan_genome': genomescan.scan_genome(splits, ks, DIR_encode, DIR_models, DIR_res_s)
     
-#     # find kernels of model_output>cutoff
-#     genomescan.find_kernels(ks, kernels_cutoff, DIR_res_s, DIR_kernels, reverse)
+    # find kernels of model_output>cutoff
+    if mode == 'find_kernels': genomescan.find_kernels(ks, kernels_cutoff, DIR_res_s, DIR_kernels, reverse)
 
-# #### <<<    
     
 
 # calc PPV of best kernels
-DIR_PPV = DIR_res+'PPV/'; check_and_make_dir(DIR_PPV)
-res = genomescan.calc_PPV_best_kernels(DIR_res+'kernels/', FILE_arnold)
-res.to_csv(DIR_PPV+'PPV.csv')
-
-
-# plot results 
-plots.plot_genomescan(res, DIR_PPV+'PPV')
+if mode =='calc_PPV':
+    res = genomescan.calc_PPV_best_kernels(DIR_res+'kernels/', FILE_arnold)
+    res.to_csv(DIR_PPV+'PPV.csv')
+    
+    # plot results 
+    plots.plot_genomescan(res, DIR_PPV+'PPV')
